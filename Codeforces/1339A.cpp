6c4e9081162e23477d04744d99cafa76cdfb9a5a
AbuTaher-vector <70890283+AbuTaher-vector@users.noreply.github.com>
2022-02-02 00:17:39 +0600

    Add files via upload

---
+++ Codeforces/1339A.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define nn int
+#define dd double
+#define ll long long int
+#define ss string
+#define nl '\n'
+void code()
+{
+  nn n;
+  cin>>n;
+  cout<<n<<nl;
+     
+    
+}
+int main()
+{
+  nn tt;
+  cin>>tt;
+  while(tt--) code();
+}
