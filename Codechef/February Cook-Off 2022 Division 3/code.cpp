30bee1138e89eb37bf2bda70e2cd10fbc103129b
AbuTaher-vector <70890283+AbuTaher-vector@users.noreply.github.com>
2022-02-23 19:49:16 +0600

    Lazy Salesman

--- Codechef/February Cook-Off 2022 Division 3/code.cpp
+++ Codechef/February Cook-Off 2022 Division 3/code.cpp
@@ -7,9 +7,20 @@ using namespace std;
 #define nl '\n'
 void code()
 {
-     nn a,b,c;
-     cin>>a>>b>>c;
-     cout<<max(a+c,b)<<nl;
+     nn n,m;
+     cin>>n>>m;
+     vector<nn>v(n);
+     for(nn i=0;i<n;i++) cin>>v[i];
+          sort(v.begin(),v.end());
+     reverse(v.begin(),v.end());
+     nn cnt=0,sum=0;
+     for(auto x:v)
+     {
+          cnt++;
+          sum+=x;
+          if(sum>=m) break;
+     }
+     cout<<n-cnt<<nl;
 }
 int main()
 {
