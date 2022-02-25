49042ff32a32cd5b75ffa86e619354f545a6e5a3
AbuTaher-vector <70890283+AbuTaher-vector@users.noreply.github.com>
2022-02-25 22:58:49 +0600

    Count the Notebooks

---
+++ Codechef/CodeChef Starters 26 Division 3 (Rated)/A.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define nn int
+#define dd double
+#define ll long long int
+#define ss string
+#define nl '\n'
+void code()
+{
+    nn n;
+    cin>>n;
+    cout<<n*10<<nl;
+}
+int main()
+{
+    nn tt;
+    cin>>tt;
+    while(tt--) code();
+}
