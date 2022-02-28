5d0750971b7ecb894d115135932dda5b27b9e075
AbuTaher-vector <70890283+AbuTaher-vector@users.noreply.github.com>
2022-02-28 22:39:16 +0600

    Digit Machine

---
+++ Atcoder/AtCoder Beginner Contest 241/A.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define nn int
+#define dd double
+#define ll long long int
+#define ss string
+#define nl '\n'
+int main()
+{
+    vector<nn>v(10);
+    for(nn i=0;i<10;i++) cin>>v[i];
+        cout<<v[v[v[0]]]<<nl;
+}
