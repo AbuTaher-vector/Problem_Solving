3d8d0e48c97793ff11f4e84a13a1284cf0f3d1d4
AbuTaher-vector <70890283+AbuTaher-vector@users.noreply.github.com>
2022-03-12 17:22:50 +0600

    Minimize Ordering

---
+++ Atcoder/AtCoder Beginner Contest 242/B.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define nn int
+#define dd double
+#define ll long long int
+#define ss string
+#define nl '\n'
+int main()
+{
+    ss s;
+    cin>>s;
+    sort(s.begin(),s.end());
+    cout<<s<<nl;
+    
+}
