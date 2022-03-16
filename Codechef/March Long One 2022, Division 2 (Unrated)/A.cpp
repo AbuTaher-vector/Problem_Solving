3b8785a5f5b872578a8a296bc5d1eb37b1bae7e1
AbuTaher-vector <70890283+AbuTaher-vector@users.noreply.github.com>
2022-03-16 08:57:39 +0600

    Akash and Function

---
+++ Codechef/March Long One 2022, Division 2 (Unrated)/A.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define nn int
+#define dd double
+#define ll long long int
+#define ss string
+#define nl '\n'
+void code()
+{
+    nn n;
+    cin>>n;
+    nn k=n/2;
+    if(n&1) k++;
+    cout<<k<<nl;
+}
+int main()
+{
+    nn tt;
+    cin>>tt;
+    while(tt--) code();
+}
