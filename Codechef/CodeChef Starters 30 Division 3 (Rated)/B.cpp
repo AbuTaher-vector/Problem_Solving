fca05fa377ed586b9bb8cb33b90de029866434ad
AbuTaher-vector <70890283+AbuTaher-vector@users.noreply.github.com>
2022-03-17 14:30:58 +0600

    Balanced Reversals

---
+++ Codechef/CodeChef Starters 30 Division 3 (Rated)/B.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define nn int
+#define dd double
+#define ll long long int
+#define ss string
+#define nl '\n'
+void code()
+{
+   nn n;
+   cin>>n;
+   ss s;
+   cin>>s;
+   sort(s.begin(),s.end());
+   cout<<s<<nl;
+   
+
+}
+int main()
+{
+    nn tt;
+    cin>>tt;
+    while(tt--) code();
+}
