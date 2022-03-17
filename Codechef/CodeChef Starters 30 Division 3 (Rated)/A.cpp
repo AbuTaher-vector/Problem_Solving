ef69d96c06a1aef5893615651a9eb2090cc70618
AbuTaher-vector <70890283+AbuTaher-vector@users.noreply.github.com>
2022-03-17 14:29:11 +0600

    Chef and Water Bottles

---
+++ Codechef/CodeChef Starters 30 Division 3 (Rated)/A.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define nn int
+#define dd double
+#define ll long long int
+#define ss string
+#define nl '\n'
+void code()
+{
+    nn n,x,k;
+    cin>>n>>x>>k;
+    nn a=k/x;
+    if(a<=n) cout<<a<<nl;
+    else cout<<n<<nl;
+   
+
+}
+int main()
+{
+    nn tt;
+    cin>>tt;
+    while(tt--) code();
+}
