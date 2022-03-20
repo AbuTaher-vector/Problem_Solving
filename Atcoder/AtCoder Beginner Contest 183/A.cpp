058fd1552d3d85298467dd18083d566c5d0871ec
AbuTaher-vector <70890283+AbuTaher-vector@users.noreply.github.com>
2022-03-20 17:24:06 +0600

    A - ReLU

---
+++ Atcoder/AtCoder Beginner Contest 183/A.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define nn int
+#define dd double
+#define ll long long int
+#define ss string
+int main()
+{
+    nn t;
+    cin>>t;
+    if(t>=0) cout<<t<<endl;
+    else cout<<0<<endl;
+ 
+ 
+ 
+}
+ 
