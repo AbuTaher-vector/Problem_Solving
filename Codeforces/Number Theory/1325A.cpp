7951651022ad2d19dbcd5b4d46a2105c65fc7c0b
ドラゴン <70890283+AbuTaher-vector@users.noreply.github.com>
2022-03-31 14:40:36 +0600

    A. EhAb AnD gCd

---
+++ Codeforces/Number Theory/1325A.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int tt;
+    cin>>tt;
+    while(tt--)
+    {
+        int x;
+        cin>>x;
+        cout<<x-1<<" "<<1<<endl;
+    }
+}
