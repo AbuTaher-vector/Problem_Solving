a723a6612d1dcd046ef537e3c64ded6990d34d48
ドラゴン <70890283+AbuTaher-vector@users.noreply.github.com>
2022-04-04 19:46:58 +0600

    A. FashionabLee

---
+++ Codeforces/Geometry/1369A.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t,x;
+    cin>>t;
+    while(t--)
+    {
+        cin>>x;
+        if(x%4==0) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+
+            }
+}
