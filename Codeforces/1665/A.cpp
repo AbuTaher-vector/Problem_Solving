63d5892949cdf339ec8b085d93ae21eb3167ded1
ドラゴン <70890283+AbuTaher-vector@users.noreply.github.com>
2022-04-08 22:53:42 +0600

    A. GCD vs LCM

---
+++ Codeforces/1665/A.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define nn int
+#define dd double
+#define ll long long int
+#define ss string
+#define nl '\n'
+void code()
+{
+  nn n;
+  cin>>n;
+  cout<<1<<" "<<(n-3)<<" "<<1<<" "<<1<<nl; 
+      
+}
+int main()
+{
+  
+     nn n;
+     cin>>n;
+     while(n--) code();
+}
