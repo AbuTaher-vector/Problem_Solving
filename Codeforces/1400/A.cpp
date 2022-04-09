c3aab6d0da4e3a2aa938ccc9bb01da9d65d1eb56
ドラゴン <70890283+AbuTaher-vector@users.noreply.github.com>
2022-04-09 19:37:29 +0600

    A. String Similarity

---
+++ Codeforces/1400/A.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int tt;
+    cin>>tt;
+    while(tt--)
+    {
+        int nn;
+        cin>>nn;
+        string s;
+        cin>>s;
+        for(int i=0;i<nn;i++)
+        {
+            cout<<s[nn-1];
+        }
+        cout<<endl;
+    }
+}
