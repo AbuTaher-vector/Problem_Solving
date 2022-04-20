e7c969ef110740a78a13d67776f84ebc453b1265
ドラゴン <70890283+AbuTaher-vector@users.noreply.github.com>
2022-04-21 01:52:31 +0530

    Police and Thief

---
+++ Codechef/CodeChef Starters 35 Division 4 (Rated)/c.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define nn int
+#define dd double
+#define ll long long int
+#define ss string
+#define nl '\n'
+void code()
+{
+     
+    nn x,y;
+    cin>>x>>y;
+     cout<<max(x,y)-min(x,y)<<nl;
+}
+int main()
+{
+     #ifndef ONLINE_JUDGE
+     freopen("input.txt", "r", stdin);
+     freopen("outputt.txt", "w", stdout);
+     #endif
+
+     nn tt;
+     cin>>tt;
+     while(tt--) code();
+}
