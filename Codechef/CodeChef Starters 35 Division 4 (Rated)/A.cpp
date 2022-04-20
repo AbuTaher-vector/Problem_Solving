0b62a25ca312243c622a7809deb91beebf94c182
ドラゴン <70890283+AbuTaher-vector@users.noreply.github.com>
2022-04-21 01:49:48 +0530

    Water Requirement

---
+++ Codechef/CodeChef Starters 35 Division 4 (Rated)/A.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define nn int
+#define dd double
+#define ll long long int
+#define ss string
+#define nl '\n'
+int code()
+{
+     
+    nn n;
+    cin>>n;
+    cout<<n*2<<nl;
+
+}
+int main()
+{
+     #ifndef ONLINE_JUDGE
+     freopen("input.txt", "r", stdin);
+     freopen("outputt.txt", "w", stdout);
+     #endif
+
+     nn tt;
+     cin>>tt;
+     while(tt--) code();
+}
