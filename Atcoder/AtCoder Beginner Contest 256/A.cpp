0ffddc0c03fcd9bb697a254cf33b6efb576258c1
ドラゴン <70890283+AbuTaher-vector@users.noreply.github.com>
2022-06-19 18:37:31 +0600

    2^N

---
+++ Atcoder/AtCoder Beginner Contest 256/A.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define nn int
+#define dd double
+#define ll long long int
+#define ss string
+#define nl '\n'
+int main()
+{
+     ios_base::sync_with_stdio(0); cin.tie(0);
+     #ifndef ONLINE_JUDGE
+     freopen("input.txt", "r", stdin);
+     freopen("outputtt.txt", "w", stdout);
+     #endif
+
+     nn n;
+     cin>>n;
+     ll x=pow(2,n);
+     cout<<x<<nl;
+}
