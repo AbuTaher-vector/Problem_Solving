05be6697ed05cb4aebf65747913fb8b6d9d8e848
ドラゴン <70890283+AbuTaher-vector@users.noreply.github.com>
2022-07-17 22:50:15 +0600

    B. Hungry Sequence

---
+++ Codeforces/Difficulty_1200/327B.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+void setup()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0);
+     #ifndef ONLINE_JUDGE
+     freopen("input.txt", "r", stdin);
+     freopen("output.txt", "w", stdout);
+     #endif
+}
+int main()
+{
+       setup();
+
+       int n;
+       cin>>n;
+       for(int i=n;i<n*2;i++) cout<<i<<" ";
+       	cout<<'\n';
+}
